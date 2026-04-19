5b1258b8f7506ea601a88e2bfce2b503281cf395
Yodha <arbind.singhoffice@gmail.com>
2026-04-19 11:42:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-from flask import Flask\n\napp = Flask(__name__)\n\n@app.route('/')\ndef index():\n    return "Hello, World!"\n\nif __name__ == '__main__':\n    app.run(port=5000)\n
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(port=5000)
